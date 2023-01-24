623302205ac97c9a89619ee396cdee90ce5a4ac3
MariuszWiacek <wiacekm.uber@gmail.com>
2023-01-24 07:13:26 +0000

    delete button added

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,32 +25,17 @@ $("textarea").filter(function () {
     return parseInt(this.id) > currentTime;
 }).addClass("future");
 
-// add event listener to buttons save , delete and clear
+// add event listener to button  - save to local storage and retrieve from local storage when page is refreshed
 
 $(".saveBtn").on("click", function () {
     var entryValue = $(this).prev("textarea").val();
     var entryId = $(this).prev("textarea").attr("id");
-   localStorage.setItem(entryId, entryValue);})
+    localStorage.setItem(entryId, entryValue);})
 
-   //clear button
-
-   $(".delete")
-
-$("#clear").on("click", function () {
-
-        localStorage.clear();
-        $("textarea").val("");
-        $(".deleteBtn").remove();
-    });
+$("textarea").each(function () {
+    var entryId = $(this).attr("id");
+    var entryValue = localStorage.getItem(entryId);
+    $(this).val(entryValue);})
 
 
 
-
-
-// mmoment.js , 
-
-// DOM Manipulation
-
-// contitional loop
-
-//create timeblocks
